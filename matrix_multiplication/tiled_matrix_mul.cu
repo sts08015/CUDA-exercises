c74d45d0aaeee1793729450b198803751eb241db
sts08015 <sts08015@gmail.com>
2023-02-02 21:13:52 +0900

    mystery

--- matrix_multiplication/tiled_matrix_mul.cu
+++ matrix_multiplication/tiled_matrix_mul.cu
@@ -1,23 +1,49 @@
+//Mystery: how come the performance of the tiling algorithm is worse than the basic one? hmm....
+//maybe matrix size isn't big enough
+
 #include <iostream>
 #include <random>
 #include <cmath>
 
 enum {FAIL, SUCCESS};
 
-__global__ void matrix_mul(const int* mat1, const int* mat2, int* res, const int n)
-{
-  int row = blockIdx.y * blockDim.y + threadIdx.y;
-  int col = blockIdx.x * blockDim.x + threadIdx.x;
+#define SM_LEN 1024
+//32*32
 
+__global__ void tiled_matrix_mul(const int* mat1, const int* mat2, int* res, const int n)
+{
+  __shared__ int SM1[SM_LEN]; //shared with all threads in a block. CTA-private.
+  __shared__ int SM2[SM_LEN];
+
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  int row = by * blockDim.y + ty;
+  int col = bx * blockDim.x + tx;
+  int tile_size = blockDim.x;
+
+  int tmp = 0;
   if(row < n && col < n)
   {
-    int sum = 0;	//utilizing variable sum instead of directly initializing res[row*n+col] significantly improves the performance (up to twice! according to my google colab environment)
-    for(int k=0;k<n;k++)
+    int sm_idx = ty * tile_size + tx;
+    for(int i=0;i<n;i+=tile_size)
     {
-      sum += mat1[row*n + k] * mat2[k*n + col]; 
+      SM1[sm_idx] = mat1[i + tx + row*n];
+      SM2[sm_idx] = mat2[(i + ty)*n + col];
+      __syncthreads();  //to assure SM1 and SM2 for a corresponding thread block are completely filled
+
+      for(int j=0;j<tile_size;j++)
+      {
+        tmp += SM1[ty*tile_size + j] * SM2[j*tile_size + tx];
+      }
+      __syncthreads();  //to assure multiplying elements in SM is done before loading next values from device memory
     }
-    res[row*n + col] = sum;
+
+    res[row*n + col] = tmp;
   }
+  
 }
 
 void init_matrix(int* mat, const int len)
@@ -74,17 +100,17 @@ int main(void)
   cudaDeviceProp props;
   cudaGetDeviceProperties(&props, devId);
   
-  const int BLOCK_SIZE = (int)sqrt(props.maxThreadsPerBlock);
-  const int GRID_SIZE = (int)ceil(n/BLOCK_SIZE);
+  const int BLOCK_SIZE = (int)sqrt(props.maxThreadsPerBlock); //32
+  const int GRID_SIZE = (int)ceil(n/BLOCK_SIZE);  //32
 
   dim3 grid(GRID_SIZE,GRID_SIZE);
   dim3 threads(BLOCK_SIZE,BLOCK_SIZE);
 
-  matrix_mul <<<grid, threads>>>(d_a,d_b,d_c,n);
+  tiled_matrix_mul <<<grid, threads>>>(d_a,d_b,d_c,n);
 
   cudaMemcpy(h_c,d_c,size,cudaMemcpyDeviceToHost);
 
-  if(verifier(h_a,h_b,h_c,n) == SUCCESS) std::cout<<"YAY!" << std::endl;
+  if(verifier(h_a,h_b,h_c,n) == SUCCESS) std::cout<< "YAY!" << std::endl;
   else std::cout << "Hmm.." << std::endl;
 
   delete[] h_a;
